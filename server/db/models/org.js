54ab01f0f79b4bd4dc2f6d5cf7774db86d807908
Christian Everett <ceverett@gmail.com>
2015-11-12 09:30:45 -0800

    added query methods to models, closes #28

--- server/db/models/org.js
+++ server/db/models/org.js
@@ -1,3 +1,11 @@
+//
+// Exports:
+// 
+// Org
+//  .addOrg( name )  -->  returns true
+//  .addUserToOrg( username, orgName, isAdmin )  -->  returns true
+//
+
 var mongoose = require('mongoose');
 
 var orgSchema = mongoose.Schema({
@@ -9,3 +17,48 @@ var orgSchema = mongoose.Schema({
 var Org = mongoose.model('Org', orgSchema);
 
 module.exports = Org;
+
+module.exports.addOrg = function (name) {
+
+  // addOrg('HR')
+
+  // Returning true - should this be something else?
+
+  var newOrg = new Org({
+    name: name,
+    users: [],
+    admins: []
+  });
+
+  newOrg.save(function (err, newOrg) {
+    if (err) {
+      throw err;
+    } else {
+      return true;
+    }
+  });
+};
+
+module.exports.addUserToOrg = function (username, orgName, isAdmin) {
+
+  // Returning true;
+
+  var org = Org.findOne({ name: orgName }).exec(function (err, found) {
+    if (err) {
+      throw err;
+    } else {
+      if (isAdmin) {
+        found.admins.push(username);
+      } else {
+        found.users.push(username);
+      }
+      found.save(function (error, result) {
+        if (error) {
+          throw error;
+        } else {
+          return true;
+        }
+      })
+    }
+  });
+};
