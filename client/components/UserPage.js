a8a5af26981449eb6f2e9cd696b4a6450831748a
Hack Reactor Students <accounts@hackreactor.com>
2015-11-12 12:20:26 -0800

    Set up basic flux architecture

--- client/components/UserPage.js
+++ client/components/UserPage.js
@@ -1,7 +1,33 @@
 var React = require('react');
 var Link = require('react-router').Link;
+var actions = require('../actions');
+var store = require('../store');
 
 var UserPage = React.createClass({
+
+  updateUser: function() {
+    this.setState({
+      user: store.getStore().user
+    });
+  },
+
+  buttonClicked: function() {
+    actions.loadUser();
+  },
+
+  componentDidMount: function() {
+    store.addChangeListener(this.updateUser);
+  },
+
+  componentWillUnmount: function(){
+    store.removeChangeListener(this.updateUser);
+  },
+
+  getInitialState: function() {
+    return {
+      user: store.getStore().user
+    }
+  },
 
   render: function() {
 
@@ -9,6 +35,8 @@ var UserPage = React.createClass({
       <div>
         <h1>This is the UserPage</h1>
         <Link to="/org">Click to go back to OrgPage</Link>
+        <div onClick={this.buttonClicked}> Click to change user</div>
+        <div>Current user is: {this.state ? this.state.user: 'no state'}</div>
       </div>
     )
   }
---
+++ client/store.js
@@ -0,0 +1,39 @@
+var assign = require('object-assign');
+var dispatcher = require('./dispatcher');
+var EventEmitter = require('events').EventEmitter;
+
+var CHANGE_EVENT = 'change';
+
+var _store = {
+  user: 'kurt'
+};
+
+var updateUser = function(data) {
+  _store.user = data.user;
+};
+
+var store = assign({}, EventEmitter.prototype, {
+  addChangeListener: function(cb){
+    this.on(CHANGE_EVENT, cb);
+  },
+  removeChangeListener: function(cb){
+    this.removeListener(CHANGE_EVENT, cb);
+  },
+  getStore: function(){
+    return _store;
+  }
+});
+
+dispatcher.register(function(payload){
+  var action = payload.action;
+  switch(action.actionType){
+    case 'UPDATE_USER':
+      updateUser(action.data);
+      store.emit(CHANGE_EVENT);
+      break;
+    default:
+      return true;
+  }
+});
+
+module.exports = store;
